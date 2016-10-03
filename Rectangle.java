5654b1dac103529b622d0fdd2541c41c09299431
201311185강성규 <tj5192@naver.com>
2016-10-03 15:09:48 +0900

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,20 @@
+class Rectangle {
+	private int width;
+	private int height;
+	public Rectangle(int w, int h) {
+		this.width = w;
+		this.height = h;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public int calcArea() {
+		return width*height;
+	}
+}
+
+Rectangle r = new Rectangle(6,7);
+System.out.println("¸éÀû: "+r.calcArea());
