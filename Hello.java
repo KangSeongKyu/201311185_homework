f64ecf6e64503de926e4220d0bc58b4255f6d14e
201311185강성규 <tj5192@naver.com>
2016-10-14 01:49:56 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,7 @@
+class Hello {
+String W = "world";
+Hello() {}
+void sayHello() {
+System.out.println("Hello "+W);
+}
+}
---
+++ Person.java
@@ -0,0 +1,13 @@
+class Person {
+	private float weight;
+	public void setWeight(float w) {
+		this.weight = w;
+	}
+	private float getWeight() {
+		return weight;
+	}
+}
+
+Person me = new Person();
+me.setWeight((float)65.0);
+System.out.println(me.getWeight());
