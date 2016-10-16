eedf84f1b3b216f904fc063c76b6b70ba1d53030
201311185강성규 <tj5192@naver.com>
2016-10-16 23:11:19 +0900

    Add files via upload

---
+++ Dice Project/Dice.java
@@ -0,0 +1,10 @@
+class dice {
+	private int number=0;
+	public void roll(){
+		number=(int)(Math.random()*6)+1;
+		System.out.println("Turns on "+number);
+	}
+	public int getNum() {
+		return this.number;
+	}
+}
