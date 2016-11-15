045049693f2c9aa9fbc9a8284a5f10d8c4b2fa6e
201311185강성규 <tj5192@naver.com>
2016-11-16 01:40:01 +0900

    Add files via upload

---
+++ 161114homework/SaveAndDraw.java
@@ -0,0 +1,17 @@
+import ch.aplu.turtle.*;
+
+class SaveAndDraw {
+  static Turtle t1 = new Turtle();
+  
+  static void drawSquareFrom(double[][] tracks) {
+    for(double[] t : tracks) {
+      t1.moveTo(t[0],t[1]);
+    }
+  }
+  
+  public static void main(String[] args) {
+    double[][] tracks = {{100,100},{200,100},{200,200},{200,100},{100,100}};
+    
+    drawSquareFrom(tracks);
+  }
+}
