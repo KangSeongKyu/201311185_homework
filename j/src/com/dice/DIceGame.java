2f3199d9b4c500b361d8cc40f3892b0521533dfa
201311185강성규 <tj5192@naver.com>
2016-12-11 16:20:00 +0900

    Add files via upload

---
+++ j/src/com/dice/DIceGame.java
@@ -0,0 +1,54 @@
+package com.sd.dice;
+
+class DiceGame{
+  private Player p1;
+  private Player p2;
+  private int point1=0;
+  private int point2=0;
+  
+  public DiceGame(String one, String two) {
+    p1 = new Player(one);
+    p2 = new Player(two);
+    
+    for(int i=0; i<2; i++){
+      p1.play();
+      p2.play();
+      startGame();
+      System.out.println(" ");
+    }
+    result();
+  }
+  public void result() {
+    if(point1 > point2) {
+      System.out.println("Player1 "+p1.getName() + "'s total point : " + point1);
+      System.out.println("Player2 "+p2.getName() + "'s total point : " + point2);
+      System.out.println(" ");
+      System.out.println("Player1 "+p1.getName() + " win!!!!!!");
+      System.out.println("Player2 "+p2.getName() + " lose......");
+    }else if (point1 == point2) {
+        System.out.println("Player1 "+p1.getName() + "'s total point : " + point1);
+        System.out.println("Player2 "+p2.getName() + "'s total point : " + point2);
+        System.out.println(" ");
+        System.out.println("draw~~~~~~~");
+    }else {
+        System.out.println(p2.getName() + "'s total point : " + point2);
+        System.out.println(p1.getName() + "'s total point : " + point1);
+        System.out.println(" ");
+        System.out.println(p2.getName() + " win!!!!!!");
+        System.out.println(p1.getName() + " lose......");   
+      }  
+   }
+   public void startGame() {
+      if(p1.getNum() > p2.getNum() || p1.getNum()==p2.getNum()) {
+        System.out.println("Player1 "+p1.getName()+"'s Dice Number is "+p1.getNum());
+        System.out.println("Player2 "+p2.getName()+"'s Dice Number is "+p2.getNum());
+        point1+=p1.getNum();
+        point2+=p2.getNum();
+      }else if(p1.getNum() < p2.getNum()){
+        System.out.println("Player2 "+p2.getName()+"'s Dice Number is "+p2.getNum());
+        System.out.println("Player1 "+p1.getName()+"'s Dice Number is "+p1.getNum());
+        point1+=p1.getNum();
+        point2+=p2.getNum();
+      }
+   }
+}
